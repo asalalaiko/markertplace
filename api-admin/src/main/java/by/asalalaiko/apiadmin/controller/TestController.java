f3b661fdc7e687134ce4be12be69bd27d70ec829
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2023-03-20 02:27:16 +0300

    add gateway

--- api-admin/src/main/java/by/asalalaiko/apiadmin/controller/TestController.java
+++ api-admin/src/main/java/by/asalalaiko/apiadmin/controller/TestController.java
@@ -1,9 +1,11 @@
 package by.asalalaiko.apiadmin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class TestController {
 
     @GetMapping("/test")
