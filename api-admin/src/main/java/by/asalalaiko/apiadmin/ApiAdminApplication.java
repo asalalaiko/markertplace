24a3400688f2a46d457f31539ec2937a88c301d2
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2023-03-20 00:45:21 +0300

    add eureka client(Admin API)

---
+++ api-admin/src/main/java/by/asalalaiko/apiadmin/ApiAdminApplication.java
@@ -0,0 +1,16 @@
+package by.asalalaiko.apiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAdminApplication.class, args);
+    }
+
+}
